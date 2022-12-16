ee58fd7885aecd65e79174fdc0a94b014a0d3cad
AndersonGomezAza <argomeza@itc.edu.co>
2022-12-16 12:43:25 -0500

    Subiendo Caracteristicas de EcmaScript 11

---
+++ src/ES11/02-Null-Ish.js
@@ -0,0 +1,3 @@
+const anotherNumber = null;
+const validate = anotherNumber ?? 5; //Comprueba si el valor es nulo y gracias a esto puede asignarse algo
+console.log(validate);
---
+++ src/ES11/04-Global-This.js
@@ -0,0 +1,4 @@
+console.log(window); //Este es para el navegador
+console.log(global); // Este es para node
+console.log(selft); //WebWorker
+console.log(globalthis);
